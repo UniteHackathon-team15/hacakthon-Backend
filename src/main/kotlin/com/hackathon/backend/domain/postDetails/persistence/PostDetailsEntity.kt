41f6c5c0409e7599251c0d8dcbc0b7e03ecb6fc3
woonseok105 <yws3786@gmail.com>
2023-10-12 15:23:35 +0900

    modify

--- src/main/kotlin/com/hackathon/backend/domain/postDetails/persistence/PostDetailsEntity.kt
+++ src/main/kotlin/com/hackathon/backend/domain/postDetails/persistence/PostDetailsEntity.kt
@@ -4,7 +4,6 @@ import com.hackathon.backend.domain.post.persistence.PostEntity
 import javax.persistence.EmbeddedId
 import javax.persistence.Entity
 import javax.persistence.FetchType
-import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 import javax.persistence.MapsId
