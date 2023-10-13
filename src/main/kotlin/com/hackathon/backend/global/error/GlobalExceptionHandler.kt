b831c877c8375cb204e2e09cec02425d2c67db8d
woonseok105 <yws3786@gmail.com>
2023-10-13 10:20:50 +0900

    modify exceptionHandler

--- src/main/kotlin/com/hackathon/backend/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/hackathon/backend/global/error/GlobalExceptionHandler.kt
@@ -6,7 +6,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.validation.BindException
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
+@RestControllerAdvice
 class GlobalExceptionHandler {
     @ExceptionHandler(BindException::class)
     fun handleBindException(e: BindException): ResponseEntity<*>? {
